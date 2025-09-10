e0ac30b6309cf132efdb2e226db75141b383c7f6
Venkata Reddy Janapala <venkatareddyjanapala@Venkatas-MacBook-Pro.local>
2025-09-10 22:04:06 +0530

    Spring boot setup for todolist

--- src/main/java/com/todolist/practise/models/Task.java
+++ src/main/java/com/todolist/practise/models/Task.java
@@ -4,16 +4,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Data;
 
 import java.time.LocalDate;
 
 
 @Entity
+@Data
+@Builder
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String taskName;
     private LocalDate startTime;
     private Status status;
--- src/main/java/com/todolist/practise/service/Impl/TodoListServiceImpl.java
+++ src/main/java/com/todolist/practise/service/Impl/TodoListServiceImpl.java
@@ -1,15 +1,25 @@
 package com.todolist.practise.service.Impl;
 
 import com.todolist.practise.dto.CreateToDoList;
+import com.todolist.practise.models.Task;
 import com.todolist.practise.repository.TaskRepository;
 import com.todolist.practise.service.TodoListService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class TodoListServiceImpl implements TodoListService {
-    private TaskRepository taskRepository;
+    private final TaskRepository taskRepository;
     @Override
     public void createList(CreateToDoList request) {
+        Task task = Task.builder()
+                .taskName(request.taskName())
+                .startTime(request.startTime())
+                .createdBy(request.createdBy())
+                .build();
 
+        taskRepository.save(task);
     }
 }
